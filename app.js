67732bbb23998259e04762efcf31757e81983e14
elvinmanzi02 <elvinmanzi@gmail.com>
2023-08-14 18:19:23 +0200

    bug fixed, api changed and css added

--- app.js
+++ app.js
@@ -14,13 +14,24 @@ async function getWeather() {
 
     try {
 
-        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=154e5dff6dc142acbaf112503232707&q=${location}&aqi=yes`, { mode: 'cors' });
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=7302873955924156a9d151922230908&q=${location}&aqi=yes`, { mode: 'cors' });
         const data = await response.json();
         console.log(data);
 
         const giphyUrl = data.current.condition.icon;
         const text = data.current.condition.text;
         const time = data.location.localtime;
+        const humidity = data.current.humidity;
+        const wind = data.current.wind_degree;
+        const pressure = data.current.pressure_in;
+        const latitude = data.location.lat;
+
+        const otherInfoElement = document.querySelector("#otherInfo");
+        otherInfoElement.innerHTML = `<b>Humidity:</b> ${humidity}% </br > 
+                                      <b>Wind :</b> ${wind}&deg </br>
+                                      <b>Pressure:</b> ${pressure}inHg </br>
+                                      <b>Latitude: </b>${latitude}&deg`
+
 
         const locationElement = document.querySelector("#location");
         location = location.charAt(0).toUpperCase() + location.slice(1).toLowerCase();
@@ -35,13 +46,12 @@ async function getWeather() {
         const celciusElement = document.querySelector("#celciusDisplay");
         const celcius = data.current.temp_c;
 
-        const fahrenheitElement = document.querySelector("#fahneitDisplay");
         const fahrenheit = data.current.temp_f;
 
         const celciusBtn = document.querySelector("#celciusBtn");
         const fahrenheitBtn = document.querySelector("#fahneitBtn");
 
-        celciusElement.innerHTML = `${celcius} &deg;C `;
+        celciusElement.innerHTML = `<p style = "font-size: 50px; margin: 0px; padding: 0px">${celcius}</p> &deg;C`;
 
         const giphyDisplayElement = document.querySelector("#gifDisplay");
         giphyDisplayElement.src = giphyUrl;
@@ -51,11 +61,11 @@ async function getWeather() {
     
 
         celciusBtn.addEventListener("click", () =>{
-            celciusElement.innerHTML = `${celcius} &deg;C `;
+            celciusElement.innerHTML = `<p style = "font-size: 50px; margin: 0px; padding: 0px">${celcius}</p> &deg;C `;
         })
 
         fahrenheitBtn.addEventListener("click", () =>{
-            celciusElement.innerHTML = `${fahrenheit} &deg;F `;
+            celciusElement.innerHTML = `<p style = "font-size: 50px; margin: 0px; padding: 0px">${fahrenheit}</p> &deg;F`;
         })
 
 
